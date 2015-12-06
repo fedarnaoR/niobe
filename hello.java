a6052e44ef12df84dd6e2423782a52e240faa90e
Federico Arnao <farnao@vbote.com>
2015-12-06 19:43:27 +0100

    Initial hello

---
+++ hello.java
@@ -0,0 +1,20 @@
+public class Hello {
+	private Integer id;
+	private String name;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
